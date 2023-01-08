8bfdb1bd5bdece9a9bd0819147ddc2bf16ac01c2
SaluSam <sonate083@gmail.com>
2023-01-08 09:20:06 -0600

    modified main.py

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 
-destinations_list = [park,pool,]
+destinations_list = ["Chicago","New York City","Orlando","Los Angeles"]
+
+restaurants_list_new_york = ["Carmine's","Balthazar","Buddakan","Keens's Steakhouse"]
+
+restaurants_list_orlando = ["The Melting Pot", "Hard Rock Cafe","Maggiano's","Charley's Steakhouse"]
+
+restaurants_list_chicago = ["Giordanos","Lou Malnatis","Gibsons Bar & Steakhouse","Frontera Grill"]
+
+restaurants_list_los_angeles = ["Bottega Louie","Musso and Frank Grill","Providence","Bestia"]
+
+transportation_list = ["Train","Car","Walk","Bike"]
+
+entertainment_list_chicago = ["Millennium Park","Museum of Contemporary Art","Grant Park","Promontory Point"]
+
+entertainment_list_new_york = ["Central Park","Frick Collection","Conservatory Garden","St. Patrick's Cathedral"]
+
+entertainment_list_orlando = ["Disney World","Sea World","Universal Studios","Discovery Cove"]
+
+entertainment_list_los_angeles = ["Getty Center","Griffith Park","Universal Studios","Hollywood Sign"]
