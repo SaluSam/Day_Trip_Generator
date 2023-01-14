69993d2977b512486bb7a55945005c03352374ec
SaluSam <sonate083@gmail.com>
2023-01-14 13:25:22 -0600

    modified main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# These are all the trip variables
+
+import random
 
 destinations_list = ["Chicago","New York City","Orlando","Los Angeles"]
 
@@ -18,3 +21,60 @@ entertainment_list_new_york = ["Central Park","Frick Collection","Conservatory G
 entertainment_list_orlando = ["Disney World","Sea World","Universal Studios","Discovery Cove"]
 
 entertainment_list_los_angeles = ["Getty Center","Griffith Park","Universal Studios","Hollywood Sign"]
+
+
+print("Welcome to Day Trip Generator. Vacation plans in just a few minutes!")
+
+user_input = ""
+
+
+while user_input != "y":
+    destination = random.choice(destinations_list)
+    user_input = input(f"Your destination is {(destination)}. Does this sound good? y/n")
+
+user_input = ""
+
+if destination == "Chicago":
+    while user_input != "y":
+        restaurant = random.choice(restaurants_list_chicago)
+        user_input = input(f"Your restaurant is {(restaurant)}. Does this sound good? y/n")
+elif destination == "New York City":
+    while user_input != "y":
+        restaurant = random.choice(restaurants_list_new_york)
+        user_input = input(f"Your restaurant is {(restaurant)}. Does this sound good? y/n")
+elif destination == "Orlando":
+    while user_input != "y":
+        restaurant = random.choice(restaurants_list_orlando)
+        user_input = input(f"Your restaurant is {(restaurant)}. Does this sound good? y/n")
+elif destination == "Los Angeles":
+    while user_input != "y":
+        restaurant = random.choice(restaurants_list_los_angeles)
+        user_input = input(f"Your restaurant is {(restaurant)}. Does this sound good? y/n")
+
+user_input = ""
+
+if destination == "Chicago":
+    while user_input != "y":
+        entertainment = random.choice(restaurants_list_chicago)
+        user_input = input(f"Your entertainment is {(entertainment)}. Does this sound good? y/n")
+elif destination == "New York City":
+    while user_input != "y":
+        entertainment = random.choice(restaurants_list_new_york)
+        user_input = input(f"Your entertainment is {(entertainment)}. Does this sound good? y/n")
+elif destination == "Orlando":
+    while user_input != "y":
+        entertainment = random.choice(restaurants_list_orlando)
+        user_input = input(f"Your entertainment is {(entertainment)}. Does this sound good? y/n")
+elif destination == "Los Angeles":
+    while user_input != "y":
+        entertainment = random.choice(restaurants_list_los_angeles)
+        user_input = input(f"Your entertainment is {(entertainment)}. Does this sound good? y/n")
+ 
+user_input = ""
+
+while user_input != "y":
+    transportation = random.choice(transportation_list)
+    user_input = input(f"Your transportation is {(transportation)}. Does this sound good? y/n")
+
+
+
